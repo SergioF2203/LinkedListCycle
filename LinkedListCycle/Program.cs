41d87c91faf0bbc1efeb9e7adba39cc175ea58da
SergioF <fs220381@gmail.com>
2020-03-29 22:24:39 +0300

    implement HasCycle Method

--- LinkedListCycle/Program.cs
+++ LinkedListCycle/Program.cs
@@ -10,7 +10,16 @@ namespace LinkedListCycle
     {
         static void Main(string[] args)
         {
+            var ln1 = new ListNode(3);
+            var ln2 = new ListNode(2);
+            var ln3 = new ListNode(0);
+            var ln4 = new ListNode(-4);
+            ln1.next = ln2;
+            ln2.next = ln1;
+            //ln3.next = ln4;
+            //ln4.next = ln2;
 
+            Solution.HasCycle(ln1);
         }
     }
 }
--- LinkedListCycle/Solution.cs
+++ LinkedListCycle/Solution.cs
@@ -8,5 +8,23 @@ namespace LinkedListCycle
 {
     class Solution
     {
+        public static bool HasCycle(ListNode head)
+        {
+            if (head == null || head.next == null) return false;
+
+            var hashTableNodes = new HashSet<ListNode>();
+            hashTableNodes.Add(head);
+
+            var nextNode = head.next;
+            while (true)
+            {
+                hashTableNodes.Add(nextNode);
+                if (nextNode.next != null)
+                    nextNode = nextNode.next;
+                else return false;
+                if (hashTableNodes.Contains(nextNode)) return true;
+                if (nextNode.next == head) return true;
+            }
+        }
     }
 }
