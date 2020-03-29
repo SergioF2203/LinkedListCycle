6f7036c5644faf2d360691d811b7c95a4360853e
SergioF <fs220381@gmail.com>
2020-03-29 14:44:11 +0300

    + implementation ListNode class

--- LinkedListCycle/ListNode.cs
+++ LinkedListCycle/ListNode.cs
@@ -8,5 +8,12 @@ namespace LinkedListCycle
 {
     class ListNode
     {
+        public int val;
+        public ListNode next;
+        public ListNode(int x)
+        {
+            val = x;
+            next = null;
+        }
     }
 }
